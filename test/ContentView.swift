9c67411a6621a155db1fb013b246deafc475c957
looloolalaa <dlrlgus65@nate.com>
2022-08-18 19:43:59 +0900

    save UIImage in document

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -7,53 +7,95 @@
 
 import SwiftUI
 
+struct TempImage: Identifiable {
+    var id: Int
+    var name: String
+    var image: Image?
+}
+
 struct ContentView: View {
+    let tempImages: [TempImage] = {
+        let fileManager = FileManager()
+        let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            let allFileURLs = try fileManager.contentsOfDirectory(at: documentURL, includingPropertiesForKeys: nil)
+            
+            var tempImages: [TempImage] = []
+            for (i, url) in allFileURLs.enumerated() {
+                let name = url.lastPathComponent
+                var image: Image?
+                if let data = try? Data(contentsOf: url), let loaded = UIImage(data: data) {
+                    image = Image(uiImage: loaded)
+                } else {
+                    image = nil
+                }
+                let tempImage = TempImage(id: i, name: name, image: image)
+                tempImages.append(tempImage)
+            }
+            
+            return tempImages
+            
+        } catch {
+            print("Error Reading File: \(error.localizedDescription)")
+        }
+        
+        return []
+    }()
+    
     var body: some View {
         VStack {
-            //no stroke
-            Button(action: {
-
-            }) {
-                Text("hello my name is")
-                    .font(.title2)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(.red)
-                    .clipShape(Capsule())
+            HStack {
+                Button("save") {
+                    let newFileName = "부엉이"
+                    let newFileContent = UIImage(imageLiteralResourceName: newFileName)
+                    
+                    let fileManager = FileManager()
+                    let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+                    let dataURL = documentURL.appendingPathComponent(newFileName)
+                    
+                    do {
+                        let imageData = newFileContent.pngData()
+                        if let _ = imageData {
+                            print("not nil")
+                        } else {
+                            print("!nil!")
+                        }
+                        try imageData?.write(to: dataURL)
+                    } catch {
+                        print("Error Writing File: \(error.localizedDescription)")
+                    }
+                }
+                
+                Button("delete") {
+                    let fileManager = FileManager()
+                    let documentURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+                    do {
+                        let allFileURLs = try fileManager.contentsOfDirectory(at: documentURL, includingPropertiesForKeys: nil)
+                        for url in allFileURLs {
+                            try fileManager.removeItem(at: url)
+                        }
+                    } catch {
+                        print("Error Deleting File: \(error.localizedDescription)")
+                    }
+                }
             }
             
-            //no background
-            Button(action: {
-                print("Hello button tapped!")
-            }) {
-                Text("Hello World")
-                    .fontWeight(.bold)
-                    .font(.title)
-                    .foregroundColor(.purple)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 45)
-                            .stroke(Color.purple, lineWidth: 5)
-                    )
+            List {
+                ForEach(tempImages) { item in
+                    HStack {
+                        Text(item.name)
+                            .bold()
+                        Divider()
+                        item.image?
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        
+                    }
+                    
+                }
             }
-            
-            //All Yes
-            Button(action: {
-                print("sign up bin tapped")
-            }) {
-                Text("SIGN UP")
-                    .fontWeight(.bold)
-                    .font(.title)
-                    .foregroundColor(.purple)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color.black, lineWidth: 5)
-                    )
-            }
-            .background(Color.yellow) // If you have this
-            .cornerRadius(25)         // You also need the cornerRadius here
-            
         }
     }
 }
