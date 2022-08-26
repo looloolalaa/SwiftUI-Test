9ec11ee299772b041ee779268b517e5eed586d4c
looloolalaa <dlrlgus65@nate.com>
2022-08-26 16:35:54 +0900

    toolbar textfield focus problem

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -7,19 +7,98 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var text = ["", ""]
+struct SearchRootView: View {
+    
+    @FocusState var focused: Field? // define here
+
+    var body: some View {
+        NavigationView {
+            NavigationLink {
+                SearchTextFieldPushView(focused: _focused) // pass down here
+            } label: {
+                Text("Search")
+            }
+        }
+    }
+}
+
+enum Field {
+    case name
+}
+
+struct SearchTextFieldPushView: View {
+    
+    @FocusState var focused: Field?
+    
+    @State var username: String = ""
+
     
     var body: some View {
         VStack {
-            TextField("name", text: $text[0])
-            TextField("name", text: $text[1])
+            Button {
+                focused = nil
+            } label: {
+                Text("Remove Focus")
+            }
+            
+            Button("Focus") {
+                focused = .name
+            }
+            
+            if self.focused == nil {
+                Text("No")
+            } else {
+                Text("Focus")
+            }
+
+            
         }
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                TextField("user name", text: $username)
+                    .focused(self.$focused, equals: Field.name)
+                    .disableAutocorrection(true)
+                    .padding(4)
+                    .border(.secondary)
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ContentView: View {
+    @FocusState private var titleFieldFocused: Field?
+//    @FocusState private var textFieldFocused: Bool
+    
+    @State private var title = "hello"
+//    @State private var text = "ffff"
+    
+    var body: some View {
+        SearchRootView()
+//        VStack {
+//            Text("hi")
+//            if (titleFieldFocused != nil) {
+//                Button("OK") {
+//
+//                }
+//            }
+//            TextField("title", text: $title)
+//                .focused($titleFieldFocused, equals: .name)
+//        }
+//        .navigationBarTitleDisplayMode(.inline)
+//        .toolbar {
+//            ToolbarItem(placement: .principal) {
+//                TextField("title", text: $title)
+//                    .focused($titleFieldFocused, equals: .name)
+//            }
+//        }
     }
 }
 
 struct test_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
