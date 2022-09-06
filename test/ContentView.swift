39b7d5674bb3888aa85de217d055b6acc98d2d48
looloolalaa <dlrlgus65@nate.com>
2022-09-06 16:00:25 +0900

    @State init

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -7,58 +7,24 @@
 
 import SwiftUI
 
-class Memos: ObservableObject {
-    @Published var items: [String] = ["0","4","3"]
-}
 
-struct ThirdView: View {
-    @ObservedObject var memos: Memos
-    var body: some View {
-        VStack {
-            HStack {
-                ForEach(memos.items, id: \.self) { item in
-                    Text(item)
-                }
-            }
-            
-            Button("change") {
-                memos.items[2] = "wow!"
-            }
-        }
-    }
-}
 
 struct SecondView: View {
-    @ObservedObject var memos: Memos
+    @State var str: String
+    
+    init(str: String) {
+        _str = State(initialValue: str)
+    }
     var body: some View {
-        NavigationView {
-            VStack {
-                HStack {
-                    ForEach(memos.items, id: \.self) { item in
-                        Text(item)
-                    }
-                }
-                NavigationLink(destination: ThirdView(memos: memos)) {
-                    Text("Go")
-                }
-            }
-        }
+        Text(str)
     }
 }
 
 struct ContentView: View {
-    @StateObject var memos: Memos = Memos()
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    ForEach(memos.items, id: \.self) { item in
-                        Text(item)
-                    }
-                }
-                NavigationLink(destination: SecondView(memos: memos)) {
-                    Text("Go")
-                }
+            NavigationLink(destination: SecondView(str: "init")) {
+                Text("Go")
             }
         }
     }
