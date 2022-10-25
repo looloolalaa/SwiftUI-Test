5c56fc13fb3cd1b150b9a90f3031e7af0e0111c9
looloolalaa <dlrlgus65@nate.com>
2022-10-25 21:16:59 +0900

    timer

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -8,14 +8,15 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var currentDate = Date()
+    @State var timeRemaining = 10
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        Text("\(currentDate)")
-            .onReceive(timer) { input in
-                currentDate = input
-                print("zz")
+        Text("\(timeRemaining)")
+            .onReceive(timer) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
             }
     }
 }
