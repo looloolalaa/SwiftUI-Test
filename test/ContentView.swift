f9c4245a11b057c1e230f6cb5a61e1c9d366ffcd
looloolalaa <dlrlgus65@nate.com>
2022-10-25 21:22:23 +0900

    count down

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -12,12 +12,16 @@ struct ContentView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        Text("\(timeRemaining)")
-            .onReceive(timer) { _ in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
+        if timeRemaining > 0 {
+            Text("\(timeRemaining)")
+                .onReceive(timer) { _ in
+                    if timeRemaining > 0 {
+                        timeRemaining -= 1
+                    }
                 }
-            }
+        } else {
+            Image(systemName: "moon")
+        }
     }
 }
 
